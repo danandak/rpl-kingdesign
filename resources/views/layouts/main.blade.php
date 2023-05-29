511beadf66d31ef6890b09aff8ba555af000762a
danandak <danandanurwendaadikusuma_1313621017@mhs.unj.ac.id>
2023-05-29 16:03:19 +0700

    Update favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+  {{-- favicon --}}
+  <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/logo.png') }}">
+
   {{-- Bootstrap CSS --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -21,7 +24,7 @@
   <link rel="stylesheet" href="/assets/css/register.css">
   <link rel="stylesheet" href="/assets/css/about.css">
 
-  {{-- <title>Document</title> --}}
+  <title>KingDesign</title>
   
 </head>
 
